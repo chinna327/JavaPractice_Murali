f046455c7815e6e8f6b23005402839270acdd838
chinna327 <vmm.murali@gmail.com>
2018-10-06 15:33:20 -0400

    nothin

---
+++ src/javapractice/StringCombi2.java
@@ -0,0 +1,17 @@
+package javapractice;
+
+public class StringCombi2 {
+	public static void main(String[] args) {
+			String Str = "ABC";
+			 StringBuilder s = new StringBuilder();
+
+			for (int i =0; i < Str.length()-1; i++){
+				
+			}
+	}
+	
+	
+	
+		
+
+}
