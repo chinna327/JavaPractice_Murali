b2e473e0ae15d8999ccf650fa5754483e1aba6d9
n0259443 <n0259443@VDDK03X-4CEIMXZ.lm.lmig.com>
2018-11-15 05:49:04 -0500

    reversestring with Inbuild function

---
+++ src/javapractice/ReverseStringNonRecursive.java
@@ -0,0 +1,15 @@
+package javapractice;
+
+import java.util.Scanner;
+
+public class ReverseStringNonRecursive {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String str = s.next();
+		StringBuffer rev = new StringBuffer(str);
+		rev.reverse();
+		System.out.println(rev);
+		}
+
+}
