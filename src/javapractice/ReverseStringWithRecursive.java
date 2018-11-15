a752f09cae2b7643402f74aced50d4f22022c6eb
n0259443 <n0259443@VDDK03X-4CEIMXZ.lm.lmig.com>
2018-11-15 08:24:55 -0500

    Reverse String with Recursive

---
+++ src/javapractice/ReverseStringWithRecursive.java
@@ -0,0 +1,25 @@
+package javapractice;
+
+import java.util.Scanner;
+
+public class ReverseStringWithRecursive {
+	static String  rev = " ";
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the String");
+		String str = s.nextLine();
+		String rev = reverseString(str);
+		System.out.println("reverseed is "+rev);
+	}
+	
+	static String reverseString(String st){		
+		
+	 if (st.length()>0){			
+		rev = rev + st.charAt(st.length()-1);
+		reverseString(st.substring(0, st.length()-1));	
+					}
+				
+		return  rev;
+	}
+
+}
