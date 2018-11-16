7dfc3ef6e4b68954b1d2a68f8edb6c9c8cbae2ba
n0259443 <n0259443@VDDK03X-4CEIMXZ.lm.lmig.com>
2018-11-16 09:34:27 -0500

    ReArrage arrays alternatively with postive and negative values.

--- src/javapractice/arrayRearrange.java
+++ src/javapractice/arrayRearrange.java
@@ -6,27 +6,63 @@ public class arrayRearrange {
 
 	public static void main(String[] args) {
 		
-		int [] a = {1,2,3,-1,-2,-3,-4};
+		//int [] a = {1,2,3,-1,-2,-3,-4,2,31,-10,-12,-11,-15};
+		int [] a = {1,2,3,-1,2,-3,4,2,31,-10,12,11,15};
 		int pivot = 0;
 		Arrays.sort(a);
-		
+		System.out.println("After sort");
 		for(int ara: a) {
 			System.out.println(ara);
 			if (ara<0) {
 				pivot++;
 			}			
 		}
-		System.out.println(pivot);
-		int [] neg = new int [pivot-1];
-		int [] pos = new int [a.length -pivot-1];
-		int [] out = new int [a.length-1];
-		int ind = 0;
-		for(int i=0; i<pivot*2; i++) {
-			 out[ind] =neg[i];
-			 out[ind++]=pos[i];
+		int [] neg = Arrays.copyOfRange(a, 0, pivot);
+		int [] pos = Arrays.copyOfRange(a, pivot, a.length);
+		int [] out = new int [a.length];
+		
+		//part 0- find which is small array
+		int small=0;
+		if (pos.length>neg.length) {
+			 small = neg.length;
+		}else {
+			 small = pos.length;
 		}
+			
+		
+		//Part 1 - enter all postivie alternatively
+		int indexo=0;
+		for(int i=0;i<small;i++) {
+			out[indexo]= pos[i];
+			indexo++;
+			indexo++;
+		}
+		//Part 2 - enter all negative alternatively
+		indexo=1;
+		for(int i=0;i<small;i++) {
+			out[indexo]= neg[i];
+			indexo++;
+			indexo++;
+		}
+		indexo--;
+			
+		//Part 3 - enter all postivie/negative remaining directly	
+		
+		if (pos.length>neg.length) {
+			for(int i=indexo;i<a.length;i++) {
+				out[i]= pos[small];
+				small++;
+			}
+		}else {
+			for(int i=indexo;i<a.length;i++) {
+				out[i]= neg[small];
+				small++;
+			}
+		}
+		
+		System.out.println("Output is ");
 		for(int ara: out) {
-			System.out.println(ara);
+			System.out.println(+ara);
 			
 		}
 
