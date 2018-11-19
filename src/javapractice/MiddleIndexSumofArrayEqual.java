6d89727ab4503a959503517570a5346baf311656
n0259443 <n0259443@VDDK03X-4CEIMXZ.lm.lmig.com>
2018-11-19 08:31:21 -0500

    index

---
+++ src/javapractice/MiddleIndexSumofArrayEqual.java
@@ -0,0 +1,49 @@
+package javapractice;
+
+public class MiddleIndexSumofArrayEqual {
+
+	public static void main(String[] args) {
+		int [] n = {2,4,4,5,4,1};
+		int index =0;
+		boolean flg = false;
+		
+		while (flg == false) {
+		flg = find(n,index);
+		
+		
+		index++;
+		
+		};
+		index--;
+		
+		System.out.println("index is "+ index);
+		
+		
+	}
+
+	static boolean find(int[] n, int index){
+		
+		int start = index;
+		int end = n.length- index-1;
+		int cs = 0, ce =0;
+		for (int i=0;i<start; i++) {
+			
+			cs = cs+n[i];
+		}
+		
+		for (int i=n.length-1;i>=start; i--) {
+			
+			ce = ce+n[i];
+		}
+		
+		if(cs==ce) {
+				return true;
+		}else {
+			return false;
+		}
+		
+	
+		
+		
+	}
+}
